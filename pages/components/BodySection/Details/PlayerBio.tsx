1597c46ebc33d4f0a44b3bd17139bb9c796be756
Derek Avila <derekavila1086@gmail.com>
2024-02-15 09:20:04 -0600

    pfp update

--- pages/components/BodySection/Details/PlayerBio.tsx
+++ pages/components/BodySection/Details/PlayerBio.tsx
@@ -3,72 +3,81 @@ import classes from "./PlayerBio.module.css";
 import { Player } from "../../..";
 
 import Image from "next/image";
-import Austro from "./bioImages/Austro.png";
-import Kalani from "./bioImages/Kalani.png";
 import DefaultIcon from "./bioImages/DefaultIcon.png";
-import Giveup from "./bioImages/Giveup.jpg";
-import Hotspot from "./bioImages/Hotspot.jpg";
-import Peprika from "./bioImages/Peprika.png";
-import Perc from "./bioImages/Perc.png";
-import August from "./bioImages/August.png";
-import AdamTheMeta from "./bioImages/AdamTheMeta.png";
-import BaconIsAwesome from "./bioImages/BaconIsAwesome.png";
-import Charmy from "./bioImages/Charmy.png";
-import chrome from "./bioImages/chrome.png";
-import Clear from "./bioImages/Clear.png";
-import Ed from "./bioImages/Ed.png";
-import Frewbay from "./bioImages/Frewbay.png";
-import Gendalf from "./bioImages/Gendalf.png";
-import Hellameme from "./bioImages/Hellameme.png";
-import Jerry from "./bioImages/Jerry.png";
-import Jewourgie from "./bioImages/Jewourgie.png";
-import MattGS from "./bioImages/MattGS.png";
-import Rave from "./bioImages/Rave.png";
-import Ghost from "./bioImages/Ghost.png";
-import GreenDan from "./bioImages/GreenDan.png";
-import Inglorian from "./bioImages/Inglorian.png";
-import ItalianTurtle from "./bioImages/ItalianTurtle.png";
-import Pilot from "./bioImages/Pilot.png";
-import Power from "./bioImages/Power.png";
-import Svenypoo from "./bioImages/Svenypoo.png";
-import VHA from "./bioImages/VHA.png";
-import Warden from "./bioImages/Warden.png";
-import DannyG from "./bioImages/DannyG.png";
-import Gustav from "./bioImages/Gustav.png";
-import KaputBasket3192 from "./bioImages/KaputBasket3192.png";
-import Okaykoay from "./bioImages/Okaykoay.png";
-import VG from "./bioImages/VG.png";
 import _7nation from "./bioImages/7nation.png";
+import AdamTheMeta from "./bioImages/AdamTheMeta.png";
 import Almondo from "./bioImages/Almondo.png";
 import Alphastar from "./bioImages/Alphastar.png";
 import Amy from "./bioImages/Amy.png";
+import August from "./bioImages/August.png";
+import Austro from "./bioImages/Austro.png";
+import BaconIsAwesome from "./bioImages/BaconIsAwesome.png";
+import Bandej from "./bioImages/Bandej.png";
 import Billjet from "./bioImages/Billjet.png";
 import Cerb from "./bioImages/Cerb.png";
 import Charleslee from "./bioImages/Charleslee.png";
+import Charmy from "./bioImages/Charmy.png";
+import chrome from "./bioImages/chrome.png";
+import Clear from "./bioImages/Clear.png";
 import crazy from "./bioImages/crazy.png";
+import DannyG from "./bioImages/DannyG.png";
+import Dorri from "./bioImages/Dorri.png";
+import Duncan from "./bioImages/Duncan.png";
 import Ecual from "./bioImages/Ecual.png";
+import Felton from "./bioImages/Felton.png";
 import Fires from "./bioImages/Fires.png";
-import frankiemcnutslap from "./bioImages/frankiemcnutslap.png";
+import Frankiemcnutslap from "./bioImages/Frankiemcnutslap.png";
+import Frewbay from "./bioImages/Frewbay.png";
+import Ghost from "./bioImages/Ghost.png";
+import Gendalf from "./bioImages/Gendalf.png";
+import Giveup from "./bioImages/Giveup.jpg";
+import GreenDan from "./bioImages/GreenDan.png";
+import Gustav from "./bioImages/Gustav.png";
+import Ed from "./bioImages/Ed.png";
+import Hellameme from "./bioImages/Hellameme.png";
 import Highlander from "./bioImages/Highlander.png";
+import Hotspot from "./bioImages/Hotspot.jpg";
+import Inglorian from "./bioImages/Inglorian.png";
+import ItalianTurtle from "./bioImages/ItalianTurtle.png";
 import JaysonH from "./bioImages/JaysonH.png";
+import Jerry from "./bioImages/Jerry.png";
+import Jewourgie from "./bioImages/Jewourgie.png";
+import Kalani from "./bioImages/Kalani.png";
+import KaputBasket3192 from "./bioImages/KaputBasket3192.png";
+import Keth from "./bioImages/Keth.png";
 import Lobcity from "./bioImages/Lobcity.png";
+import Lynx from "./bioImages/Lynx.png";
 import Madbox from "./bioImages/Madbox.png";
+import MattGS from "./bioImages/MattGS.png";
 import Mattrobes from "./bioImages/Mattrobes.png";
 import MilkMan from "./bioImages/MilkMan.png";
 import moxeetwo from "./bioImages/moxeetwo.png";
-import Nine from "./bioImages/Nine.png";
 import NoahMcMoney from "./bioImages/NoahMcMoney.png";
+import Nine from "./bioImages/Nine.png";
 import Notacoolguy from "./bioImages/Notacoolguy.png";
+import Okaykoay from "./bioImages/Okaykoay.png";
 import Paddy from "./bioImages/Paddy.png";
+import Peffryping from "./bioImages/peffryping.png";
+import Peprika from "./bioImages/Peprika.png";
+import Perc from "./bioImages/Perc.png";
+import Pilot from "./bioImages/Pilot.png";
+import Power from "./bioImages/Power.png";
 import PRC from "./bioImages/PRC.png";
-import Sock from "./bioImages/Sock.png";
+import Rave from "./bioImages/Rave.png";
 import Reddishh from "./bioImages/Reddishh.png";
-import russell from "./bioImages/russell.png";
+import Robo from "./bioImages/Robo.png";
+import Russell from "./bioImages/Russell.png";
+import Shmadie from "./bioImages/Shmadie.png";
+import Sock from "./bioImages/Sock.png";
 import Spicypaper from "./bioImages/Spicypaper.png";
 import Stryker from "./bioImages/Stryker.png";
+import Svenypoo from "./bioImages/Svenypoo.png";
 import Thad from "./bioImages/Thad.png";
-import toasted from "./bioImages/toasted.png";
+import Toasted from "./bioImages/Toasted.png";
 import ToasterCoaster from "./bioImages/ToasterCoaster.png";
+import VG from "./bioImages/VG.png";
+import VHA from "./bioImages/VHA.png";
+import Warden from "./bioImages/Warden.png";
 import WifiBills from "./bioImages/WifiBills.png";
 
 interface PlayerBioProps {
@@ -91,138 +100,159 @@ const PlayerBio: React.FC<PlayerBioProps> = ({
 
   const findImage = (name: string) => {
     switch (name) {
-      case "Lobcity":
-        return Lobcity;
-      case "Sock":
-        return Sock;
-      case "Wifi Bills":
-        return WifiBills;
-      case "russell":
-        return russell;
-      case "Thad":
-        return Thad;
-      case "Toasted":
-        return toasted;
-      case "Spicypaper":
-        return Spicypaper;
-      case "Notacoolguy":
-        return Notacoolguy;
-      case "Nine":
-        return Nine;
-      case "PRC":
-        return PRC;
-      case "Stryker":
-        return Stryker;
-      case "ToasterCoaster":
-        return ToasterCoaster;
-      case "Paddy":
-        return Paddy;
-      case "Reddishh":
-        return Reddishh;
-      case "Mattrobes":
-        return Mattrobes;
-      case "Madbox":
-        return Madbox;
-      case "moxeetwo":
-        return moxeetwo;
-      case "Noah McMoney":
-        return NoahMcMoney;
-      case "The Milk Man":
-        return MilkMan;
-      case "Fires":
-        return Fires;
-      case "Highlander":
-        return Highlander;
-      case "FrankieMcNutSlap":
-        return frankiemcnutslap;
-      case "JaysonH":
-        return JaysonH;
-      case "Ecual":
-        return Ecual;
-      case "crazy":
-        return crazy;
-      case "BILLJET":
-        return Billjet;
-      case "Cerb":
-        return Cerb;
-      case "Charles":
-        return Charleslee;
       case "7nation":
         return _7nation;
+      case "Adam The Meta":
+        return AdamTheMeta;
       case "Almondo":
         return Almondo;
       case "AlphaStar":
         return Alphastar;
       case "Amy":
         return Amy;
-      case "Danny G":
-        return DannyG;
-      case "Gustav":
-        return Gustav;
-      case "KaputBasket3192":
-        return KaputBasket3192;
-      case "Okaykoay":
-        return Okaykoay;
-      case "VG":
-        return VG;
-      case "Warden":
-        return Warden;
-      case "Austro":
-        return Austro;
-      case "Kalani":
-        return Kalani;
-      case "Giveup":
-        return Giveup;
-      case "Hotspot":
-        return Hotspot;
-      case "Peprika":
-        return Peprika;
-      case "Perc":
-        return Perc;
       case "Captain August":
         return August;
-      case "Adam The Meta":
-        return AdamTheMeta;
+      case "Austro":
+        return Austro;
       case "Bacon Is Awesome":
         return BaconIsAwesome;
+      case "Bandej":
+        return Bandej;
+      case "BILLJET":
+        return Billjet;
+      case "Cerb":
+        return Cerb;
+      case "Charles":
+        return Charleslee;
       case "Charmy":
         return Charmy;
       case "chrome":
         return chrome;
       case "Clear":
         return Clear;
-      case "Ed":
-        return Ed;
+      case "crazy":
+        return crazy;
+      case "Danny G":
+        return DannyG;
+      case "Dorri":
+        return Dorri;
+      case "Duncan":
+        return Duncan;
+      case "Ecual":
+        return Ecual;
+      case "Felton":
+        return Felton;
+      case "Fires":
+        return Fires;
+      case "FrankieMcNutSlap":
+        return Frankiemcnutslap;
       case "Frewbay":
         return Frewbay;
       case "Gendalf":
         return Gendalf;
-      case "HellaMeme":
-        return Hellameme;
-      case "Jerry":
-        return Jerry;
-      case "Jewourgie":
-        return Jewourgie;
-      case "MattGS":
-        return MattGS;
-      case "Rave":
-        return Rave;
-      case "GHO5T":
+      case "Giveup":
+        return Giveup;
+      case "Ghost":
         return Ghost;
       case "Green Dan":
         return GreenDan;
+      case "GHO5T":
+        return Ghost;
+      case "Gustav":
+        return Gustav;
+      case "Ed":
+        return Ed;
+      case "HellaMeme":
+        return Hellameme;
+      case "Highlander":
+        return Highlander;
+      case "Hotspot":
+        return Hotspot;
       case "Inglorian":
         return Inglorian;
       case "Italian Turtle":
         return ItalianTurtle;
+      case "JaysonH":
+        return JaysonH;
+      case "Jerry":
+        return Jerry;
+      case "Jewourgie":
+        return Jewourgie;
+      case "Kalani":
+        return Kalani;
+      case "KaputBasket3192":
+        return KaputBasket3192;
+      case "keth":
+        return Keth;
+      case "Lobcity":
+        return Lobcity;
+      case "Lynx":
+        return Lynx;
+      case "Madbox":
+        return Madbox;
+      case "MattGS":
+        return MattGS;
+      case "Mattrobes":
+        return Mattrobes;
+      case "The Milk Man":
+        return MilkMan;
+      case "moxeetwo":
+        return moxeetwo;
+      case "Nine":
+        return Nine;
+      case "Noah McMoney":
+        return NoahMcMoney;
+      case "Notacoolguy":
+        return Notacoolguy;
+      case "Okaykoay":
+        return Okaykoay;
+      case "Paddy":
+        return Paddy;
+      case "peffry ping":
+        return Peffryping;
+      case "Peprika":
+        return Peprika;
+      case "Perc":
+        return Perc;
       case "Pilot":
         return Pilot;
+      case "PRC":
+        return PRC;
       case "Power":
         return Power;
+      case "Rave":
+        return Rave;
+      case "Reddishh":
+        return Reddishh;
+      case "russell":
+        return Russell;
+      case "Robo":
+        return Robo;
+      case "Shmadie":
+        return Shmadie;
+      case "Sock":
+        return Sock;
+      case "Spicypaper":
+        return Spicypaper;
+      case "Stryker":
+        return Stryker;
       case "Svenypoo":
         return Svenypoo;
+      case "Thad":
+        return Thad;
+      case "Toasted":
+        return Toasted;
+      case "ToasterCoaster":
+        return ToasterCoaster;
       case "VHA":
         return VHA;
+      case "VG":
+        return VG;
+      case "Warden":
+        return Warden;
+      case "Wifi Bills":
+        return WifiBills;
+
       default:
         return DefaultIcon;
     }
